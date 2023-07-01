62491ecec7104615496ad8aaf5a156a368c32332
vika-tsymbaliyk <125582375+vika-tsymbaliyk@users.noreply.github.com>
2023-07-01 20:45:08 +0000

    Deploying to gh-pages from @ vika-tsymbaliyk/goit-js-hw-08@30074fa135040db6e1a7929a32d549fa3ceb2d9d 🚀

--- 03-feedback.1bb3446f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("feedback-form").addEventListener("input",saveData);
-//# sourceMappingURL=03-feedback.1bb3446f.js.map
--- 03-feedback.c9b81119.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("feedback-form").addEventListener("input",saveData);
-//# sourceMappingURL=03-feedback.c9b81119.js.map
